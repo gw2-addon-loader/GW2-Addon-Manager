68205818bf187c53346e7749f8a130b8a1e78867
FriendlyFire <jpguertin.cg@gmail.com>
2021-10-21 20:36:39 -0400

    Redesigned how UI handles check boxes for addons list. Added Select All/None buttons. App will now tick all addons with updates available on launch for easy updating.

--- application/GW2AddonManager/UI/OpeningPage/OpeningViewModel.cs
+++ application/GW2AddonManager/UI/OpeningPage/OpeningViewModel.cs
@@ -4,25 +4,40 @@ using Microsoft.Toolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
-using Ookii.Dialogs.Wpf;
 using System.Linq;
-using System.Windows.Controls;
 using System;
+using System.Collections.Generic;
 
 namespace GW2AddonManager
 {
     public class OpeningViewModel : DependentObservableObject
     {
-        private AddonInfo _selectedAddon = null;
-        private ObservableCollection<AddonInfo> _checkedAddons = new ObservableCollection<AddonInfo>();
-        private ObservableCollection<AddonInfo> _addons = new ObservableCollection<AddonInfo>();
+        public class AddonInfoCheck : ObservableObject
+        {
+            private bool _checked;
+
+            public AddonInfo Addon { get; init; }
+            public bool AddonChecked
+            {
+                get => _checked;
+                set => SetProperty(ref _checked, value);
+            }
+
+            public AddonInfoCheck(bool c, AddonInfo i)
+            {
+                _checked = c;
+                Addon = i;
+            }
+        }
+
+        private AddonInfo _selectedAddon;
         private readonly IAddonManager _addonManager;
         private readonly IAddonRepository _addonRepository;
         private readonly ICoreManager _coreManager;
+        private readonly IConfigurationProvider _configurationProvider;
 
-        public ObservableCollection<AddonInfo> CheckedAddons { get => _checkedAddons; }
-
-        public ObservableCollection<AddonInfo> Addons { get => _addons; }
+        public ObservableCollection<AddonInfoCheck> Addons { get; } = new ObservableCollection<AddonInfoCheck>();
+        public IEnumerable<AddonInfo> CheckedAddons { get => Addons.Where(x => x.AddonChecked).Select(x => x.Addon); }
 
         [DependsOn("SelectedAddon")]
         public string DescriptionText => _selectedAddon?.Description ?? StaticText.SelectAnAddonToSeeMoreInformationAboutIt;
@@ -36,8 +51,11 @@ namespace GW2AddonManager
         public AddonInfo SelectedAddon { get => _selectedAddon; set => SetProperty(ref _selectedAddon, value); }
 
 
-        [DependsOn("CheckedAddons")]
-        public bool AnyAddonChecked => CheckedAddons.Count > 0;
+        [DependsOn("Addons")]
+        public bool AnyAddonChecked => Addons.Any(x => x.AddonChecked);
+
+        [DependsOn("Addons")]
+        public bool NotAllAddonsChecked => Addons.Any(x => !x.AddonChecked);
 
         public ICommand DisableSelected => new RelayCommand(() => _addonManager.Disable(CheckedAddons));
 
@@ -47,15 +65,21 @@ namespace GW2AddonManager
 
         public ICommand CleanInstall => new RelayCommand(() => _coreManager.Uninstall());
 
-        public ICommand CheckAddon => new RelayCommand<object[]>(x => {
-            bool isChecked = (bool)x[1];
-            AddonInfo addonInfo = (AddonInfo)x[0];
-
-            if(isChecked)
-                CheckedAddons.Add(addonInfo);
-            else
-                _ = CheckedAddons.Remove(addonInfo);
+        public ICommand CheckAllAddons => new RelayCommand(() =>
+        {
+            foreach (var a in Addons)
+                a.AddonChecked = true;
+            OnPropertyChanged("Addons");
         });
+
+        public ICommand UncheckAllAddons => new RelayCommand(() =>
+        {
+            foreach (var a in Addons)
+                a.AddonChecked = false;
+            OnPropertyChanged("Addons");
+        });
+
+        public ICommand CheckAddon => new RelayCommand(() => OnPropertyChanged("Addons"));
 
         private string _log = "";
         public string Log
@@ -67,14 +91,21 @@ namespace GW2AddonManager
         [DependsOn("Log")]
         public Visibility LogVisibility => Log.Length > 0 ? Visibility.Visible : Visibility.Collapsed;
 
-        public OpeningViewModel(IAddonManager addonManager, IAddonRepository addonRepository, ICoreManager coreManager)
+        public OpeningViewModel(IAddonManager addonManager, IAddonRepository addonRepository, ICoreManager coreManager, IConfigurationProvider configurationProvider)
         {
             _addonManager = addonManager;
             _addonRepository = addonRepository;
             _coreManager = coreManager;
-            _addons = new ObservableCollection<AddonInfo>(_addonRepository.Addons.Values.OrderBy(x => x.AddonName));
+            _configurationProvider = configurationProvider;
+            Addons = new ObservableCollection<AddonInfoCheck>(_addonRepository.Addons.OrderBy(x => x.Value.AddonName).Select(
+                x => {
+                        bool check = false;
+                        if(_configurationProvider.UserConfig.AddonsState.TryGetValue(x.Key, out AddonState state))
+                            check = state.Installed && state.VersionId != x.Value.VersionId;
 
-            CheckedAddons.CollectionChanged += (_, _) => OnPropertyChanged("CheckedAddons");
+                        return new AddonInfoCheck(check, x.Value);
+                    }));
+
             _coreManager.Log += _coreManager_Log;
         }
 
